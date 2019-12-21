4531a0dbb29fdd316dcc8b8041e0c41a397703a6
soichisumi <soichi.sumi@gmail.com>
2019-12-22 03:10:14 +0900

    use javascript-action

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const wait = require('./wait');
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try { 
+    const ms = core.getInput('milliseconds');
+    console.log(`Waiting ${ms} milliseconds ...`)
+
+    core.debug((new Date()).toTimeString())
+    wait(parseInt(ms));
+    core.debug((new Date()).toTimeString())
+
+    core.setOutput('time', new Date().toTimeString());
+  } 
+  catch (error) {
     core.setFailed(error.message);
+  }
 }
+
+run()
