08128859a5fb260c1f84c5cd4e789f9bd2cc5d75
soichisumi <soichi.sumi@gmail.com>
2019-12-22 16:48:37 +0900

    implement issue copier

--- index.js
+++ index.js
@@ -1,16 +1,92 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const wait = require('./wait');
 
+const getPRNumber = () => {
+    const pr = github.context.payload.pull_request;
+    if (!pr) {
+        return undefined;
+    }
+    return pr.number;
+};
+
+const getIssueNumber = () => {
+    const issue = github.context.payload.issue;
+    if (!issue) {
+        return undefined;
+    }
+    return issue.number;
+}
+
+const getRepo = () => {
+    const repo = github.context.repo;
+    return repo;
+}
+
+const getIssue = async (token) => {
+    let g = new github.GitHub(token);
+    const issueNum = getIssueNumber();
+
+    if (!issueNum) {
+        throw new Error("No issue provided");
+    }
+    const repo = getRepo();
+    const issue = await g.issues.get({
+        owner: repo.owner,
+        repo: repo.repo,
+        num: issueNum,
+    });
+    
+    return issue;
+};
+
+const checkKeyword = (keywords, title, body) => {
+    const lowerBody = body.toLowerCase()
+    for(let k of keywords) {
+        if (k.toLowerCase().includes(k)){
+            return true;
+        }
+    }
+    return false;
+}
+
+const createNewIssue = async (token, owner, repoName, title, body, assignees, labels, fromIssue) => {
+    const octokit = new github.GitHub(token);
+    if (!!fromIssue) {
+        body = body + `\n\ncopiedFrom: ${fromIssue}`
+    }
+
+    const res = await octokit.issues.create(
+        {
+            owner: owner,
+            repo: repoName,
+            title: title,
+            body: body,
+            assignees: assignees,
+            labels: labels,
+        }
+    )
+    return [res.id, res.number].join(':')
+};
 
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const keyword = core.getInput('keyword', {required: true});
+    const repo = core.getInput('repo', {required: true}); // owner/repoName
+    const splitted = repo.split('/')
+    const owner = splitted[0];
+    const repoName = splitted[1];
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    const token = core.getInput('github-token');
+    const issue = await getIssue(token);
+
+    if (!checkKeyword([keyword], issue.data.title, issue.data.body)){
+      console.log("Keyword not included");
+      return;
+    }
+
+    await createNewIssue(token, owner, repoName, issue.data.title, 'this is body', ['soichisumi'], '', issue.data.url);
 
     core.setOutput('time', new Date().toTimeString());
   } 
@@ -19,4 +95,4 @@ async function run() {
   }
 }
 
-run()
+run().then(()=>{})
