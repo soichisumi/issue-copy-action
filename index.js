2f27e507b4c78063f6694de4fe387adcf2e2fdac
soichisumi <soichi.sumi@gmail.com>
2019-12-22 19:26:36 +0900

    update run

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ async function run() {
       return;
     }
 
-    const created = await createNewIssue(token, owner, repoName, issue.data.title, 'this is body', ['soichisumi'], '', issue.data.url);
+    const created = await createNewIssue(token, owner, repoName, issue.data.title, 'this is body', ['soichisumi'], [], issue.data.url);
 
     core.setOutput('created', created);
   } 
